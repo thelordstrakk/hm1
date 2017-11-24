55ccb07e3cffadd9dacf5349eac392fc93bc8490
Chris Seitz <strakk@chriss-mac.local>
2017-11-24 09:20:53 -0800

    Initial Commit

---
+++ Home App WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Home App WatchKit Extension
+//
+//  Created by Chris Seitz on 11/24/17.
+//  Copyright © 2017 Chris Seitz. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
